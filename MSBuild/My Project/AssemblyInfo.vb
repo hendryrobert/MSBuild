e74742ba0c070780e9a647c32f9a66cdac4c7f1d
hendryrobert <bobhendry@envisionsoft.com>
2016-02-11 09:52:44 -0600

    First Commit

---
+++ MSBuild/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("MSBuild")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("-")>
+<Assembly: AssemblyProduct("MSBuild")>
+<Assembly: AssemblyCopyright("Copyright © - 2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a2a4851b-f83d-486e-94e7-52d197672fb0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
